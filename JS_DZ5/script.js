a798e8a8a7123c8953e4f6c53d54581225e70a21
alexs19999 <alexs-alpha@yandex.ru>
2026-07-09 17:29:31 +0300

    js5

---
+++ JS_DZ5/script.js
@@ -0,0 +1,15 @@
+document.writeln("<div id='divSample'></div>");
+let divTxt = document.querySelector("#divSample");
+// let divTxt = document.getElementById("divSample");
+divTxt.textContent = 'Термоста́т — прибор для поддержания постоянной температуры. Поддержание температуры обеспечивается либо за счёт использования терморегуляторов, либо осуществлением фазового перехода (например, таяние льда). Для уменьшения потерь тепла или холода термостаты, как правило, теплоизолируют. Но не всегда. Широко известны автомобильные моторы, где летом нет никакой теплоизоляции и за счёт действия восковых термостатов поддерживается постоянная температура. Другим примером термостата, широко используемого в быту, является холодильник.';
+divTxt.style.backgroundColor = "yellow";
+divTxt.style.color = "black";
+divTxt.style.width = "256px";
+divTxt.style.height = "256px";
+divTxt.style.overflow = "scroll";
+divTxt.style.outline = "1px dashed red";
+divTxt.className = "resetFont";
+let cls = document.querySelector(".resetFont");
+cls.style.fontSize = "20pt";
+cls.style.fontweight = "400";
+cls.style.textDecoration = "underline";
